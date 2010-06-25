de3a5b8857db7ac1c3157a8c2ceff18c5849f024
Hugo Lopes Tavares <hltbra@gmail.com>
2010-06-24 23:42:48 -0300

    print is a function in py3k - added parenthesis to make it compatible
    --HG--
    branch : trunk

--- tests/a_script.py
+++ tests/a_script.py
@@ -2,15 +2,15 @@ import sys
 
 def main(args):
     if args == ['error']:
-        print >> sys.stderr, "stderr output"
+        sys.stderr.write('stderr output\n')
         return
     if args[:1] == ['exit']:
         sys.exit(int(args[1]))
     if args == ['stdin']:
-        print sys.stdin.read().upper()
+        print(sys.stdin.read().upper())
         return
     for arg in args:
-        print 'Writing %s' % arg
+        print('Writing %s' % arg)
         open(arg, 'w').write('test')
 
 if __name__ == '__main__':
