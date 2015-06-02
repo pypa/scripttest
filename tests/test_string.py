ae85dfc72f5f3fcea89cc369f06dbe0daad096dd
Kubilay Kocak <koobs@users.noreply.github.com>
2015-06-03 00:46:43 +1000

    Add reasons to skipIf's

--- tests/test_string.py
+++ tests/test_string.py
@@ -22,8 +22,8 @@ else:
     utf8_str = 'Björk Guðmundsdóttir [ˈpjœr̥k ˈkvʏðmʏntsˌtoʊhtɪr]'
 
 
-py2only = pytest.mark.skipif("sys.version_info >= (3, 0)")
-py3only = pytest.mark.skipif("sys.version_info < (3, 0)")
+py2only = pytest.mark.skipif("sys.version_info >= (3, 0)",reason="Python 2.x Only")
+py3only = pytest.mark.skipif("sys.version_info < (3, 0)",reason="Python 3.x Only")
 
 
 # -----------------------------------------
